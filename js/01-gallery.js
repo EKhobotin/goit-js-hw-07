c9ffe2635630c0f3bb3a9accaa4657fbd89f9149
EKhobotin <evgen.29.3.85@gmail.com>
2023-11-02 22:56:25 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,9 +1,7 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-console.log(galleryItems);
 //шукаємо елементи
 const listGallery = document.querySelector('.gallery');
-console.dir(listGallery);
 //функція для створення елемента
 function createGalleryItem({ preview, original, description }) {
     return `<li class="gallery__item">
@@ -22,24 +20,25 @@ function createGalleryItems(arr) {
     listGallery.insertAdjacentHTML('afterbegin',arr.map((elem) => createGalleryItem(elem)).join(''))      
 }
 createGalleryItems(galleryItems);
-
 //навішуємо слухача на спільного предка - список
 listGallery.addEventListener('click', onGalleryItemClick)
 //створюємо функцію слухача
 function onGalleryItemClick(event) {
     event.preventDefault();
     if (!event.target.nodeName === 'IMG') { return };
-    const imgUrl = event.target.dataset.source;
-    console.log(event.currentTarget);
+    const imgUrl = event.target.dataset.source;   
     const instance = basicLightbox.create(`
     <img src="${imgUrl}">
 `);
     instance.show();
 //після відкриття модалки вмикаємо слухача на Esc
-document.addEventListener('keydown', onEscDown);
+    document.addEventListener('keydown', onEscDown);
+    //функція слухача Esc
 function onEscDown(event) {
     event.preventDefault();
-    if (event.code === 'Escape') { instance.close(); console.log('hello'); };     
+    if (event.code === 'Escape') {
+        instance.close();
+    };     
 }
 }
        
